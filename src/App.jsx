756d2b7c01b0e082ac8f2614c88315c73141b531
Goutham <goutham24.2006@gmail.com>
2026-09-05 11:48:53 +0530

    Remove cookie consent banner and preferences route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import CookieConsent from "./components/CookieConsent";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import Processing from "./pages/Processing";
@@ -9,7 +8,6 @@ import Results from "./pages/Results";
 import About from "./pages/About";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
-import CookiePreferences from "./pages/CookiePreferences";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
@@ -25,12 +23,10 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms" element={<TermsAndConditions />} />
-          <Route path="/cookie-preferences" element={<CookiePreferences />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
-      <CookieConsent />
     </div>
   );
 }
