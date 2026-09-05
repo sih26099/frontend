2e8c2fc150be7a22c6ce11d850ffc10c648da26f
Goutham <goutham24.2006@gmail.com>
2026-09-05 13:45:23 +0530

    Move floating nav buttons above Netlify badge

--- src/components/FloatingNav.jsx
+++ src/components/FloatingNav.jsx
@@ -28,8 +28,7 @@ export default function FloatingNav() {
   if (!visible) return null;
 
   return (
-    <div className="fixed bottom-6 right-6 z-40 flex flex-col gap-3">
-      {location.pathname !== "/" && (
+<div className="fixed bottom-20 right-6 z-40 flex flex-col gap-3">      {location.pathname !== "/" && (
         <button
           onClick={goHome}
           aria-label="Go to home page"
